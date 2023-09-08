6a60ec2e061260ca5ec6507775ae233af9bc35c4
Hanvu2k <114717404+Hanvu2k@users.noreply.github.com>
2023-09-08 17:59:49 +0700

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,2 +1,2 @@
 export const API_URL = "http://localhost:5000/api/v1";
-export const PRODUCT_URL = "https://ecommerce-server-hqbv.onrender.com";
+export const PRODUCT_URL = "https://ecommerce-server-hqbv.onrender.com/api/v1";
